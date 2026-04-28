a63308a56ffc61a5c7a166cacbd96350472ceb22
Ranjitha-M20 <ranjithamohanraj97@gmail.com>
2026-04-28 14:56:50 +0530

    Delete Assignment2.js

--- Assignment2.js
+++
@@ -1,9 +0,0 @@
-const browserName="Chrome"
-function getBrowserName(){
-    if(browserName==="Chrome"){
-        let browserName="Firefox"
-        console.log("Inside block:",browserName)
-    }
-    console.log("outside block:",browserName)
-}
-getBrowserName()
